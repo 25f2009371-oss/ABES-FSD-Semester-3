15a2d605bd8ebd5f91fb0c132a1454556c5a1ff1
25f2009371-oss <25f2009371@ds.study.iitm.ac.in>
2026-09-08 11:43:06 +0530

    doing relevant ops in CRUD ops file

--- FSModule/synch.js
+++ FSModule/synch.js
@@ -3,4 +3,23 @@ fs.writeFileSync('secastudent.txt','This is the experiment1','utf-8')
 console.log('file is wrriten succefuly')
 
 
-const data=fs.readFileSync("secastuden.txt","utf-8")
+
+const data=fs.readFileSync("secastudent.txt","utf-8")
+console.log("File content is given as: ",data);
+
+
+
+
+
+fs.appendFileSync('secastudent.txt','\n This ther new line addesss')
+console.log('File is appended')
+
+/*
+fs.read
+
+fs.unlinkSync
+
+*/
+
+fs.mkdirSync('sample folder')
+console.log('new folder')
