1236016d12b7f22847a27eece3f1046eac8df18c
25f2009371-oss <25f2009371@ds.study.iitm.ac.in>
2026-09-08 11:29:14 +0530

    learning how to do CRUD operation in js

---
+++ FSModule/synch.js
@@ -0,0 +1,6 @@
+const fs = require('fs')
+fs.writeFileSync('secastudent.txt','This is the experiment1','utf-8')
+console.log('file is wrriten succefuly')
+
+
+const data=fs.readFileSync("secastuden.txt","utf-8")
