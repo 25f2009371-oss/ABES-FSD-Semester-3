56aee885fbc1e0ea55520a4da938705d2a777457
25f2009371-oss <25f2009371@ds.study.iitm.ac.in>
2026-09-09 09:24:08 +0530

    Adding appending function in js

--- FSModule/async.js
+++ FSModule/async.js
@@ -7,11 +7,27 @@ fs.writeFile('sample.txt', "Welcome to FSD", (err) => {
     }
     console.log('File created successfully');
 
-    fs.readFile('sample.txt', 'utf-8', (err, data) => {
+fs.readFile('sample.txt', 'utf-8', (err, data) => {
         if (err) {
             console.log('Error reading file', err);
             return;
         }
         console.log('File content:', data);
+
+        fs.appendFile('sample.txt', '\nSemester: 3', (err) => {
+            if (err) {
+                console.log('Error updating file:', err);
+            } else {
+                console.log('File updated successfully');
+
+                fs.readFile('sample.txt', 'utf-8', (err, updatedData) => {
+                    if (err) {
+                        console.log('Error reading updated file', err);
+                        return;
+                    }
+                    console.log('Updated File content:', updatedData);
+                });
+            }
+        });
     });
 });
